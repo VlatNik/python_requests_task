9a0bf7da3ee8d3c12ce48a878894812b7aaab32a
VlatNik <vlad.nikolaev.97@gmail.com>
2024-06-02 22:30:24 +0300

    - adding auth

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 import requests
+#создание сессии
+session = requests.session()
+#адрес метода и user agent
+url_login = 'https://forum.ru-board.com/misc.cgi'
+user_a = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
 
+header = {
+    'user-agent': user_a
+}
+#тело запроса
+payload = {
+    'action': 'dologin',
+    'inmembername': 'XRay13',
+    'inpassword': 'NdGnPc'
+}
 
+response = session.post(url_login, data=payload, headers=header).text
+#выгрузка страницы об успешной авторизации
+with open("success_page.html","w") as f:
+    f.write(response)
